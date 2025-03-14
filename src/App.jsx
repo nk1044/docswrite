50da848d1bbf870e3555fef78333f23591124895
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-14 22:34:58 +0530

    remove Home component; update routing to direct to Docs page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Pages/Layout';
-import Home from './Pages/Home';
 import Docs from './Pages/Docs';
 
 const router = createBrowserRouter([{
   path: '/',
   element: <Layout />,
   children: [
-    // {path: '', element: <Home />},
     {path: '', element: <Docs />},
   ]
 }]);
